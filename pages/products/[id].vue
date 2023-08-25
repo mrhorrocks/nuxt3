27985e3aea673fcdf404fccf196b8d5bf0c0be58
mrhorrocks <mr.horrocks@gmail.com>
2023-08-25 02:51:18 +0100

    route parameter example added

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        Product details for {{ id }}
+    </div>
+</template>
+
+<script setup>
+    // this must match the file name, in this case 'id'
+    const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
