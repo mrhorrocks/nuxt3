519ba241770ef52630b247049e8e0da397f98050
mrhorrocks <mr.horrocks@gmail.com>
2023-08-30 03:17:04 +0100

    tailwindcss and config file added

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#D675A9',
+        secondary: {
+          100: '#00E800',
+          100: '#00BC00'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
