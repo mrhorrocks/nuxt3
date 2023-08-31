994f7167122bebc4884df7e2166a8870e89a7461
mrhorrocks <mr.horrocks@gmail.com>
2023-08-31 01:58:00 +0100

    footer added and placed

--- layouts/custom.vue
+++ layouts/custom.vue
@@ -1,9 +1,10 @@
 <template>
-    <div>
+    <div class="flex flex-col min-h-screen">
         <TheHeader />
-        <div class="container mx-auto p-4">
+        <main class="container mx-auto p-4 grow items-start">
             Custom
             <slot />
-        </div>
+        </main>
+        <TheFooter />
     </div>
 </template>
