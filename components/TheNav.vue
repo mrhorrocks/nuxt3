d30f9cde76dfa35391e397f83b4b71a1e96d4c04
mrhorrocks <mr.horrocks@gmail.com>
2023-09-02 04:55:10 +0100

    comments updated

--- components/TheNav.vue
+++ components/TheNav.vue
@@ -17,18 +17,20 @@ export default {
   
 <template>
     <nav class="bg-blue-500 p-4">
-
         <!-- Primary Menu and Site Title -->
         <div class="container mx-auto flex justify-between items-center">
             <!-- Site Title -->
             <h1><NuxtLink to="/">Site title</NuxtLink></h1>
-            <!-- show below 1024px -->
+
+            <!-- Show below 1024px -->
             <button @click="toggleMobileMenu" class="lg:hidden text-white">
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
                     <path fill="currentColor" d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
                 </svg>
             </button>
-            <!-- hide menu below 1024px -->
+
+            <!-- MAIN MENU -->
+            <!-- Hide below 1024px -->
             <div class="lg:flex space-x-4 hidden">
                 <NuxtLink to="/" class="text-white hover:text-gray-300">Home1</NuxtLink>
                 <NuxtLink to="/products" class="text-white hover:text-gray-300">Products</NuxtLink>
@@ -39,7 +41,8 @@ export default {
             </div>
         </div>
 
-        <!-- show below 1024px -->
+        <!-- MOBILE MENU -->
+        <!-- Show below 1024px -->
         <div v-if="mobileMenuOpen" class="lg:hidden bg-blue-500">
             <NuxtLink to="/" class="block py-2 px-4 text-white">Home2</NuxtLink>
             <NuxtLink to="/products" class="block py-2 px-4 text-white">Products</NuxtLink>
