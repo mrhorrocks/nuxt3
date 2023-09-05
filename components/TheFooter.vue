e38a30a023fc77d38ba27b1ad1a50d2706288b7a
mrhorrocks <mr.horrocks@gmail.com>
2023-09-05 05:24:32 +0100

    scss file structure added

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,5 +1,12 @@
 <template>
     <footer>
-        <div class="container mx-auto p-4">footer</div>
+        <div class="container mx-auto p-4"><p>footer <span>text</span></p></div>
     </footer>
 </template>
+
+<style lang="scss" scoped>
+    p {
+        background: green;
+        span { color: white;}
+    }
+</style>
