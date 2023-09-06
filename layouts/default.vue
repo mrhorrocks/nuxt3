ec6b6d527ee9175f53a528b72cd0365d934ec5b8
mrhorrocks <mr.horrocks@gmail.com>
2023-09-06 18:24:10 +0100

    refactor styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,7 @@
 <template>
     <div class="flex flex-col min-h-screen">
         <TheHeader />
-        <main class="container mx-auto grow items-start">
-            Default
+        <main class="main">
             <slot />
         </main>
         <TheFooter />
