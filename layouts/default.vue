79e40dd75878079a52aa9a207eeddaa7e282c31d
mrhorrocks <mr.horrocks@gmail.com>
2023-09-08 03:05:03 +0100

    inline style classes sent to stylesheet

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="flex flex-col min-h-screen">
+    <div class="app-height">
         <TheHeader />
-        <main class="main container mx-auto mt-4 px-2">
+        <main>
             <slot />
         </main>
         <TheFooter />
