541536b20bc3da3995a00acee5b84fceb50deaf0
mrhorrocks <mr.horrocks@gmail.com>
2023-09-12 03:31:23 +0100

    graph page code formatting

--- pages/graphs.vue
+++ pages/graphs.vue
@@ -1,9 +1,13 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
-    Page: foo
-  </div>
+    
+    <h2 class="page-heading">Graphs</h2>
+
+    <h2>Chartjs</h2>
+    
+    <h2>Google Charts</h2>
+
 </template>
 
 <style scoped></style>
