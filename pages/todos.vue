ad057d24a0757cdda8b18fd4ce664ccea763c2e7
mrhorrocks <mr.horrocks@gmail.com>
2023-09-12 03:26:29 +0100

    todos updated

--- pages/todos.vue
+++ pages/todos.vue
@@ -1,9 +1,14 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
-    Page: foo
-  </div>
+    <h2 class="page-heading">Todos</h2>
+    <ol class="list-decimal pl-4 pt-4">
+        <li>Transfer all inline tailwind classes to SCSS</li>
+        <li>Add Chartjs examples</li>
+        <li>Add google charts examples</li>
+        <li>Add UI library component examples</li>
+        <li>Convert the todo page content to markdown</li>
+    </ol>
 </template>
 
 <style scoped></style>
