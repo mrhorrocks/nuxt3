3aefd97258423a4cdc9c46094f9077cbb75955ed
mrhorrocks <mr.horrocks@gmail.com>
2023-09-12 03:29:15 +0100

    heading style added

--- pages/about.vue
+++ pages/about.vue
@@ -3,7 +3,7 @@
         <Title>A Nuxt 3 Project - About</Title>
         <Meta name="description" content='About text' />
     </Head>
-    <h2>About</h2>
+    <h2 class="page-heading">About</h2>
     <!-- See: ~/content/about.md for details -->
     <ContentDoc />
 </template>
