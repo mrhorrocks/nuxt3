0ffbb52b1890bea292378493be8defbbd9bc1fd8
mrhorrocks <mr.horrocks@gmail.com>
2023-09-15 15:08:56 +0100

    chartfolder structure added to config

--- pages/charts.vue
+++ pages/charts.vue
@@ -1,15 +1,30 @@
-<script lang="ts" setup></script>
-
 <template>
+    <h2 class="page-heading">Charts</h2>
     
-    <h2 class="page-heading">Graphs</h2>
-
     <h3>Chartjs</h3>
 
-    <BarChart />
-    
-    <h3>Google Charts</h3>
+    <!-- row 1 -->
+    <div class="md:grid grid-cols-2">
+        <div class="pt-8">
+            <h3>Bar</h3>
+            <BarChart />
+        </div>
+        
+        <div class="pt-8">
+            <h3>Bar</h3>
+            <BarChart />
+        </div>
+    </div>
+
+    <!-- row 2 -->
+    <div class="md:grid grid-cols-2">
+        <div class="pt-8">
+            <h3>Google Charts</h3>
+        </div>
+        
+        <div class="pt-8">
+        c2
+        </div>
+    </div>
 
 </template>
-
-<style scoped></style>
