1189c696fb647d7a005e4542d5d443159a04f3e1
mrhorrocks <mr.horrocks@gmail.com>
2023-09-16 20:06:06 +0100

    basic chartjs configured

--- pages/charts.vue
+++ pages/charts.vue
@@ -1,30 +1,17 @@
 <template>
     <h2 class="page-heading">Charts</h2>
-    
     <h3>Chartjs</h3>
 
     <!-- row 1 -->
-    <div class="md:grid grid-cols-2">
-        <div class="pt-8">
-            <h3>Bar</h3>
+    <div class="lg:grid grid-cols-3">
+        <div class="col-span-2 lg:pr-32">
+            <h3 class="chart-title">UK hours of sunight by month</h3>
             <BarChart />
         </div>
-        
-        <div class="pt-8">
-            <h3>Bar</h3>
-            <BarChart />
+
+        <div class="pr-4">
+            <h3 class="chart-title">UK hours of sunight by month</h3>
+            <PieChart />
         </div>
     </div>
-
-    <!-- row 2 -->
-    <div class="md:grid grid-cols-2">
-        <div class="pt-8">
-            <h3>Google Charts</h3>
-        </div>
-        
-        <div class="pt-8">
-        c2
-        </div>
-    </div>
-
 </template>
