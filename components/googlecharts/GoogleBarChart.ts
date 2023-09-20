0358e24433995ab8c87f57fb6362cd183f1470bb
mrhorrocks <mr.horrocks@gmail.com>
2023-09-20 03:53:44 +0100

    chart data replacement

--- components/googlecharts/GoogleBarChart.ts
+++ components/googlecharts/GoogleBarChart.ts
@@ -1,40 +1,56 @@
 import { defineComponent, h } from 'vue';
 import { GChart } from 'vue-google-charts';
-export const type = 'BarChart';
+// export const type = 'BarChart'; // Horizontal
+export const type = 'ColumnChart'; // Vertical
 export const data = [
-  ['City', '2010 Population', '2000 Population'],
-  ['New York City, NY', 8175000, 8008000],
-  ['Los Angeles, CA', 3792000, 3694000],
-  ['Chicago, IL', 2695000, 2896000],
-  ['Houston, TX', 2099000, 1953000],
-  ['Philadelphia, PA', 1526000, 1517000],
+    ['Month', 'Hours', { role: 'style' }],
+    ['Jan', 8, '#92400e'],
+    ['Feb', 9, '#b45309'],
+    ['Mar', 11, '#f59e0b'],
+    ['Apr', 14, '#fcd34d'],
+    ['May', 15, '#fde68a'],
+    ['Jun', 16, '#fef3c7'],
+    ['Jul', 15, '#fde68a'],
+    ['Aug', 14, '#fcd34d'],
+    ['Sep', 12, '#fbbf24'],
+    ['Oct', 10, '#d97706'],
+    ['Nov', 6, '#451a03'],
+    ['Dec', 7, '#78350f'],
 ];
 
 export const options = {
-  title: 'Population of Largest U.S. Cities',
-  // chartArea: { width: '50%' },
-  hAxis: {
-    title: 'Total Population',
-    minValue: 0,
-  },
-  vAxis: {
-    title: 'City',
-  },
-  // width: 800,
-  height: 530,
+    // title: 'Population of Largest U.S. Cities',
+    // theme: 'maximized', // Brings everyting (x/y axis and legend) into the chart area
+    
+    'chartArea': { left: 50, top: 10, width: "100%", height: "80%" },
+    hAxis: {
+        // title: 'Total Population',
+        minValue: 0,
+    },
+    // vAxis: {
+    //     title: 'City',
+    // },
+    // width: 800,
+    height: 357,
+    backgroundColor: 'transparent',
+    legend: {position: 'in'},
+    colors:['#92400e', 'red'],
+    bar: {
+        groupWidth: '76%' // Space between columns
+    }
 };
 
 export default defineComponent({
-  name: 'GoogleChart',
-  components: {
-    GChart,
-  },
-  setup() {
-    return () =>
-      h(GChart, {
-        data,
-        options,
-        type,
-      });
-  },
+    name: 'GoogleChart',
+    components: {
+        GChart,
+    },
+    setup() {
+        return () =>
+            h(GChart, {
+                data,
+                options,
+                type,
+            });
+    },
 });
--- components/googlecharts/GooglePieChart.ts
+++ components/googlecharts/GooglePieChart.ts
@@ -2,31 +2,58 @@ import { defineComponent, h } from 'vue';
 import { GChart } from 'vue-google-charts';
 export const type = 'PieChart';
 export const data = [
-  ['Task', 'Hours per Day'],
-  ['Work', 11],
-  ['Eat', 2],
-  ['Commute', 2],
-  ['Watch TV', 2],
-  ['Sleep', 7],
+    ['Month', 'Hours per Day'],
+    ['Jan', 8],
+    ['Feb', 9],
+    ['Mar', 11],
+    ['Apr', 14],
+    ['May', 15],
+    ['Jun', 16],
+    ['Jul', 15],
+    ['Aug', 14],
+    ['Sep', 12],
+    ['Oct', 10],
+    ['Nov', 6],
+    ['Dec', 7],
 ];
 
 export const options = {
-  title: 'My Daily Activities',
-//   width: 800,
-  height: 530,
+    //  title: 'My Daily Activities',
+    // theme: 'maximized', // Brings everyting (x/y axis and legend) into the chart area
+    'legend': 'none',
+    // 'legend': {position: 'right'/* textStyle: {color: 'blue', fontSize: 16} */ },
+    'chartArea': { left: 0, top: 30, right: 0, bottom: 0, width: "100%", height: "80%" },
+    // width: '100%',
+    height: 350,
+    //   'is3D':true,
+    backgroundColor: 'transparent',
+    colors: [
+        '#92400e', // Jan
+        '#b45309', // Fen
+        '#f59e0b', // Mar
+        '#fcd34d', // Apr
+        '#fde68a', // May
+        '#fef3c7', // June - Lightest
+        '#fde68a', // Jul
+        '#fcd34d', // Aug
+        '#fbbf24', // Sep
+        '#d97706', // Oct
+        '#451a03', // Nov
+        '#78350f', // Dec
+    ],
 };
 
 export default defineComponent({
-  name: 'GoogleChart',
-  components: {
-    GChart,
-  },
-  setup() {
-    return () =>
-      h(GChart, {
-        data,
-        options,
-        type,
-      });
-  },
+    name: 'GoogleChart',
+    components: {
+        GChart,
+    },
+    setup() {
+        return () =>
+            h(GChart, {
+                data,
+                options,
+                type,
+            });
+    },
 });
