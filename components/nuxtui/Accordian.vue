49dadb432283223e16526c51fac5885617cd6fea
mrhorrocks <mr.horrocks@gmail.com>
2023-09-23 03:54:48 +0100

    example '@nuxt/ui' components added to app

---
+++ components/nuxtui/Accordian.vue
@@ -0,0 +1,17 @@
+<script setup>
+const items = [{
+    label: 'Getting Started',
+    icon: 'i-heroicons-information-circle',
+    defaultOpen: false,
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.'
+}, {
+    label: 'Installation',
+    icon: 'i-heroicons-arrow-down-tray',
+    disabled: false,
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.'
+}]
+</script>
+
+<template>
+    <UAccordion :items="items" />
+</template>
--- pages/ui-libraries.vue
+++ pages/ui-libraries.vue
@@ -7,8 +7,70 @@
     <h2 class="page-heading">UI Libraries</h2>
 
     <section aria-label="UI Libraries">
-
         <h3>Nuxt UI</h3>
+        
+        <p>A few pebuilt components using <a href="https://ui.nuxt.com/" class="underline">Nuxt UI</a></p>
 
+        <div class="ui-item">
+            <UButton>Button</UButton>
+        </div>
+        
+        <!-- Avatar -->
+        <div class="ui-item">
+            <Avatar />
+        </div>
+        
+        <!-- Accordian -->
+        <div class="ui-item">
+            <Accordian />
+        </div>
+        
+        <!-- Keyboard -->
+        <div class="ui-item">
+            <UKbd>Q</UKbd>
+            <UKbd>W</UKbd>
+            <UKbd>E</UKbd>
+            <UKbd>R</UKbd>
+            <UKbd>T</UKbd>
+            <UKbd>Y</UKbd>
+        </div>
+        
+        <!-- Checkbox -->
+        <div class="ui-item">
+            <form action="">
+                <Checkbox id="No" label="No" />
+            </form>
+        </div>
+        
+        <!-- Toggle -->
+        <div class="ui-item">
+            <Toggle />
+        </div>
+        
+        <!-- Range -->
+        <div class="ui-item">
+            <Range />
+        </div>
+        
+        <!-- Dropdown -->
+        <div class="ui-item">
+            <Dropdown />
+        </div>
     </section>
+
 </template>
+
+<style scoped>
+.ui-item {
+    padding-bottom: 20px;
+    margin-bottom: 20px;
+    border-bottom: 1px solid #b6b6b6;
+}
+
+.underline {
+    text-decoration: underline;
+    text-decoration-style: dotted;
+    text-decoration-thickness: 2px;
+}
+    
+</style>
