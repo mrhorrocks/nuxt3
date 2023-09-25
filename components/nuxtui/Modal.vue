b391de9f03ca6cce0497d3a4fca02a4bc3530bad
mrhorrocks <mr.horrocks@gmail.com>
2023-09-25 19:37:17 +0100

    new nuxtui example components added

---
+++ components/nuxtui/Modal.vue
@@ -0,0 +1,15 @@
+<script setup>
+const isOpen = ref(false)
+</script>
+
+<template>
+    <div>
+        <UButton label="Open standatd modal" @click="isOpen = true" />
+
+        <UModal v-model="isOpen" width="sm:max-w-lg" height="h-100">
+            <div class="m-10">
+                <UButton>Button</UButton>
+            </div>
+        </UModal>
+    </div>
+</template>
---
+++ components/nuxtui/SideOver.vue
@@ -0,0 +1,11 @@
+<script setup>
+const isOpen = ref(false)
+</script>
+<template>
+    <div>
+        <UButton label="Open Sidebar" @click="isOpen = true" />
+        <USlideover v-model="isOpen">
+            <!-- Content -->
+        </USlideover>
+    </div>
+</template>
