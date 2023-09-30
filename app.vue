c7c930bf369b40c655610551640184c18963342e
mrhorrocks <mr.horrocks@gmail.com>
2023-09-30 21:55:27 +0100

    consent cookie added to app

--- app.vue
+++ app.vue
@@ -1,12 +1,36 @@
 <template>
     <Body class="antialiased">
-        
-
         <NuxtLayout>
             <NuxtPage />
         </NuxtLayout>
 
+        <CookieControl locale="en">
+            <template #bar>
+                <h3>We Use Cookies</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, laborum delectus.</p>
+            </template>
 
-        <UNotifications />#
+            <template #modal>
+                <h3>Cookie Options</h3>
+                <p>Changed your mind about the cookies we set?</p>
+            </template>
+        </CookieControl>
+
+        <UNotifications />
     </Body>
 </template>
+
+<style>
+.cookieControl__ModalContent {
+    border-radius: 2rem;
+    overflow: hidden;
+}
+
+.cookieControl__ModalClose {
+    display: none;
+}
+
+.cookieControl__ModalButtons {
+    margin-top: 0;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     // This solves a vue bug when calling <ModalFull /> 
-    vue: {  
+    vue: {
         compilerOptions: {
-          isCustomElement: (tag) => ['Placeholder'].includes(tag),
+            isCustomElement: (tag) => ['Placeholder'].includes(tag),
         },
-      },
+    },
     app: {
         // Global title and meta config 
         head: {
@@ -22,8 +22,8 @@ export default defineNuxtConfig({
         '@pinia/nuxt',
         // '@nuxtjs/tailwindcss',
         '@nuxt/ui',
+        '@dargmuesli/nuxt-cookie-control'
     ],
-
     // Add a global css file
     css: [
         "@/assets/css/global.css"
@@ -32,9 +32,8 @@ export default defineNuxtConfig({
     //     // https://content.nuxtjs.org/api/configuration
     // },
     devtools: {
-        enabled: true, // Show devtools in the browser
+        enabled: false, // Show devtools in the browser
     },
-
     components: [
         // Add sub folders to the component direcory 
         { path: '~/components/chartjs', pathPrefix: false },
@@ -47,5 +46,101 @@ export default defineNuxtConfig({
         // ~/components/Btn.vue => <Btn />
         // ~/components/base/Btn.vue => <BaseBtn />
         '~/components'
-    ]
+    ],
+    cookieControl: {
+        // typed module options
+        // Position of cookie bar.
+        // 'top-left', 'top-right', 'top-full', 'bottom-left', 'bottom-right', 'bottom-full'
+        barPosition: 'bottom-full',
+
+        // Switch to toggle if clicking the overlay outside the configuration modal closes the modal.
+        closeModalOnClickOutside: true,
+
+        // Component colors.
+        // If you want to disable colors set colors property to false.
+        colors: {
+            barBackground: 'gold',
+            barButtonBackground: 'yellow',
+            barButtonColor: 'pink',
+            barButtonHoverBackground: 'green',
+            barButtonHoverColor: 'orange',
+            barTextColor: 'purple',
+            checkboxActiveBackground: 'blue',
+            checkboxActiveCircleBackground: '#fff',
+            checkboxDisabledBackground: '#ddd',
+            checkboxDisabledCircleBackground: '#fff',
+            checkboxInactiveBackground: '#000',
+            checkboxInactiveCircleBackground: '#fff',
+            controlButtonBackground: '#fff',
+            controlButtonHoverBackground: '#000',
+            controlButtonIconColor: '#000',
+            controlButtonIconHoverColor: '#fff',
+            focusRingColor: '#808080',
+            modalBackground: '#fff',
+            modalButtonBackground: '#000',
+            modalButtonColor: '#fff',
+            modalButtonHoverBackground: '#333',
+            modalButtonHoverColor: '#fff',
+            modalOverlay: '#000',
+            modalOverlayOpacity: 0.5,
+            modalTextColor: '#000',
+            modalUnsavedColor: '#fff',
+        },
+
+        // The cookies that are to be controlled.
+        // See detailed explanation further down below!
+        cookies: {
+            necessary: [],
+            optional: [],
+        },
+
+        // The milliseconds from now until expiry of the cookies that are being set by this module.
+        cookieExpiryOffsetMs: 1000 * 60 * 60 * 24 * 365, // one year
+
+        // Names for the cookies that are being set by this module.
+        cookieNameIsConsentGiven: 'ncc_c',
+        cookieNameCookiesEnabledIds: 'ncc_e',
+
+        // Options to pass to nuxt's useCookie
+        cookieOptions: {
+            path: '/',
+        },
+
+        // Switch to toggle the "accept necessary" button.
+        isAcceptNecessaryButtonEnabled: true,
+
+            // Switch to toggle the button that opens the configuration modal. (Cookie options icon)
+            isControlButtonEnabled: true,
+
+        // Switch to toggle the concatenation of target cookie ids to the cookie description.
+        isCookieIdVisible: false,
+
+        // Switch to toggle the inclusion of this module's css.
+        // If css is set to false, you will still be able to access your color variables.
+        // Disable css if you want to write your own.
+        isCssEnabled: false,
+
+        // Switch to toggle the css variables ponyfill.
+        isCssPonyfillEnabled: false,
+
+        // Switch to toggle the separation of cookie name and description in the configuration modal by a dash.
+        isDashInDescriptionEnabled: true,
+
+        // Switch to toggle the blocking of iframes.
+        // This can be used to prevent iframes from adding additional cookies.
+        isIframeBlocked: false,
+
+        // Switch to toggle the modal being shown right away, requiring a user's decision.
+        isModalForced: false,
+
+        // The locales to include.
+        locales: ['en'],
+
+        // Translations to override.
+        localeTexts: {
+            en: {
+                save: 'Remember',
+            }
+        }
+    },
 })
