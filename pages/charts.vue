74cf89168de6d333c2286afffce68d1c3e46e142
mrhorrocks <mr.horrocks@gmail.com>
2023-10-04 01:57:55 +0100

    page grid updated

--- pages/charts.vue
+++ pages/charts.vue
@@ -3,21 +3,27 @@
         <Title>Charts - Mr.Horrocks</Title>
         <Meta name="description" content='Chart Libraries' />
     </Head>
-    
+
     <h2 class="page-heading">Charts</h2>
-    
+
     <section aria-label="Chartjs Examples">
-        <h3 class="section-title"><a href="https://www.chartjs.org/">Chartjs Examples</a></h3>
-        <div class="lg:grid grid-cols-3">
-            <div class="col-span-2 lg:pr-32">
-                <h3 class="chart-title">UK Sunlight Hours</h3>
-                <ChartjsBarChart />
+        <div class="lg:grid grid-cols-3 gap-12">
+
+            <div class="col-span-2">
+                <h3 class="section-title">
+                    <a href="https://www.chartjs.org/" class="underline">Chartjs Examples</a>
+                </h3>
+                <div>
+                    <h3 class="chart-title">UK Sunlight Hours</h3>
+                    <ChartjsBarChart />
+                </div>
             </div>
 
-            <div class="pr-4">
+            <div class="col-span-1 mt-12">
                 <h3 class="chart-title">UK Sunlight Hours</h3>
                 <ChartjsPieChart />
             </div>
+
         </div>
     </section>
 </template>
