5eedc06874e508b783fb49db5e695ba8d42b6e8b
mrhorrocks <mr.horrocks@gmail.com>
2023-10-04 01:52:51 +0100

    button with new states added

---
+++ components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+    <button :class="buttonClass">
+        <slot>{{ linkText }}</slot>
+    </button>
+</template>
+
+<script>
+export default {
+    props: {
+        activeClass: {
+            type: String,
+            default: 'active',
+        },
+        buttonClass: {
+            type: String,
+            default: '',
+        },
+        linkText: {
+            type: String,
+            required: false,
+            default: 'add text'
+        },
+    },
+};
+</script>
+  
