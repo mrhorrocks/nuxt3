8a93fc602fbdc7f701a37d50fac711032b489cef
mrhorrocks <mr.horrocks@gmail.com>
2023-10-18 22:39:33 +0100

    click test working

---
+++ layouts/blank.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="app-height">
+        <main>
+            <ResponsiveMenu />
+            <slot />
+        </main>
+        <TheFooter />
+    </div>
+</template>
---
+++ pages/test.vue
@@ -0,0 +1,35 @@
+<script setup>
+definePageMeta({
+    layout: 'blank'
+});
+</script>
+
+<template>
+    <div>
+        123456
+    </div>
+</template>
+
+
+  
+<script>
+export default {
+  data() {
+    return {
+      menuOpen: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.menuOpen = !this.menuOpen;
+    },
+    closeMenu() {
+      this.menuOpen = false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* You can style the menu here using Tailwind CSS classes */
+</style>
