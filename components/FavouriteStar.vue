2e210cb2aed24a56b2b54eaa9820847d6a93a82b
mrhorrocks <mr.horrocks@gmail.com>
2023-11-07 03:23:24 +0000

    star colour updated

--- components/FavouriteStar.vue
+++ components/FavouriteStar.vue
@@ -1,14 +1,6 @@
 <template>
     <div @click.stop.prevent="toggleFavorite">
-        
         <i :class="['fa', { 'fa-star': isFavorite, 'fa-star-o': !isFavorite }]"></i>
-        
-        <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-            stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-star">
-            <polygon
-                points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
-        </svg>
-
     </div>
 </template>
   
@@ -33,7 +25,7 @@ export default {
     display: inline-block;
     width: 20px;
     height: 20px;
-    color: #7572FF;
+    color: #ff0000;
     font-size: 1.3rem;
     cursor: pointer;
 }
