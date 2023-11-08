558f5075bb16d942e2c6e0fe36b4e435acd83504
mrhorrocks <mr.horrocks@gmail.com>
2023-11-08 13:42:52 +0000

    Meta and title updated

--- pages/cars.vue
+++ pages/cars.vue
@@ -1,10 +1,12 @@
 <template>
     <Head>
-        <Title>TESTING</Title>
-        <Meta name="description" content='Testing' />
+        <Title>Cars</Title>
+        <Meta name="description" content='Cars' />
     </Head>
 
-    <h2 class="page-heading">Testing</h2>
+    <h2 class="page-heading">Car Component</h2>
+
+    <p>Populated using JSON data.</p>
 
     <section aria-label="Testing Examples">
 
