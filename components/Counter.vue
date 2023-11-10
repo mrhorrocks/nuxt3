4d864d87b94aedf833a228947ba4d2da917f8015
mrhorrocks <mr.horrocks@gmail.com>
2023-11-10 01:54:24 +0000

    useState test

---
+++ components/Counter.vue
@@ -0,0 +1,25 @@
+<script setup>
+const counter = useState('counter', () => Math.round(Math.random() * 1000))
+</script>
+
+<template>
+    <div>
+        <span class="count">
+            Counter: {{ counter }}
+        </span>
+
+        <button class="button" @click="counter++">
+            +
+        </button>
+        <button class="button" @click="counter--">
+            -
+        </button>
+    </div>
+</template>
+
+
+<style scoped>
+.count {
+    display: block;
+}
+</style>
