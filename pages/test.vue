8bd2d9fb73b4b2abbee7c4c1639524c93fb4ce13
mrhorrocks <mr.horrocks@gmail.com>
2023-11-14 14:04:19 +0000

    test page added

--- pages/test.vue
+++ pages/test.vue
@@ -1,20 +1,9 @@
 <script setup>
-const timestamp = 1677679162168; // Replace this with your timestamp
-const date = new Date(timestamp);
 
-const year = date.getFullYear();
-const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
-const day = date.getDate().toString().padStart(2, '0');
-const hours = date.getHours().toString().padStart(2, '0');
-const minutes = date.getMinutes().toString().padStart(2, '0');
-const seconds = date.getSeconds().toString().padStart(2, '0');
-
-const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-console.log(formattedDate);
+const convertTimeStamp = useConvertTimeStamp();
 
 // found @ ~/composables/usePrintString.ts
 const printString = usePrintString(); // Call the usePrintString plugin
-
 
 </script>
 
@@ -35,6 +24,10 @@ const printString = usePrintString(); // Call the usePrintString plugin
 
         <p>
             {{ printString }}
+        </p>
+
+        <p>
+            {{ convertTimeStamp }}
         </p>
         <!-- END-GRID -->
 
