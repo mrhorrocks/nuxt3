4f0823f9510c55be9a10cb877fc3f31beb34ad74
mrhorrocks <mr.horrocks@gmail.com>
2023-12-03 02:31:47 +0000

    extra input fields added

--- pages/cms-two.vue
+++ pages/cms-two.vue
@@ -1,4 +1,8 @@
 <template>
+    <Head>
+        <Title>CMS2 - Mr.Horrocks</Title>
+        <Meta name="description" content='Convert to JSON' />
+    </Head>
     <div class="mt-20">
         <form @submit.prevent="handleSubmit">
             <TextField
@@ -7,56 +11,58 @@
             label="Page Title" 
             placeholder="Add a title" 
             v-model="formData.pagetitle" 
-            
             />
 
-        <TextField 
+            <TextField 
             type="text"
             id="sub-heading" 
             label="Sub Heading" 
             placeholder="Sub-heading"
             v-model="formData.subheading"
-            
             />
 
-        <TextArea  
+            <TextArea  
             id="main-copy"
             label="Main Copy" 
             placeholder="Write your stuff here..." 
             v-model="formData.maincopy"
-            
             />
 
-            <!-- <select id="selectOption" v-model="formData.selectedOption">
-                <option value="option1">Option 1</option>
-                <option value="option2">Option 2</option>
-                <option value="option3">Option 3</option>
-            </select> -->
-            
-            <!-- <SelectOptions id="selectOption" label="Orgs" v-model="formData.selectedOption">
-                <option value="option3">Option 3</option>
-            </SelectOptions> -->
-
+            <br>
             <Radio value="Mr." name="name" v-model="formData.radioGroup" label="Mr" id="mr" />
             <Radio value="Mrs." name="name" v-model="formData.radioGroup" label="Mrs" id="mrs" />
             <Radio value="Ms" name="name" v-model="formData.radioGroup" label="Ms" id="ms" />
             
-            <!--
-            <input type="radio" v-model="formData.radioGroup" value="group1"> Group 1
-            <input type="radio" v-model="formData.radioGroup" value="group2"> Group 2
-            <input type="radio" v-model="formData.radioGroup" value="group3"> Group 3
-            -->
+            <br>
 
-            <div>
-                <label>
-                    <input type="checkbox" v-model="formData.checkbox1"> Checkbox 1
-                </label>
-            </div>
-            <div>
-                <label>
-                    <input type="checkbox" v-model="formData.checkbox2"> Checkbox 2
-                </label>
-            </div>
+            <label class="checkbox">
+                <input type="checkbox" v-model="formData.checkbox1" name="checkbox1" />
+                <span class="checkmark"></span>
+                <span class="checkboxLabel">C1</span>
+            </label>         
+
+            <label class="checkbox">
+                <input type="checkbox" v-model="formData.checkbox2" name="checkbox2" />
+                <span class="checkmark"></span>
+                <span class="checkboxLabel">C2</span>
+            </label>
+            
+            <br>
+
+            <TextField
+            type="number"
+            id="age" 
+            label="Age" 
+            placeholder="Enter a number" 
+            v-model="formData.age" 
+            />
+        
+            <br>
+
+            <SelectOptions id="selectOption" v-model="formData.selectedOption" />
+        
+            <br>
+
             <Button class="button submit" type="submit" text="Submit" />
         </form>
 
@@ -73,9 +79,10 @@ export default {
                 subheading: '',
                 maincopy: '',
                 selectedOption: '',
+                age: '',
                 radioGroup: '',
-                checkbox1: false,
-                checkbox2: false,
+                checkbox1: '',
+                checkbox2: '',
             },
             submitted: false,
             submittedData: null,
